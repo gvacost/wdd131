ecc84353fa6c192a070ea4fee4f0996b4a3a98d1
gvacosta <gonza.96.acosta@gmail.com>
2024-06-20 14:49:48 -0300

    js bom commit

---
+++ week02/scripts/bom.js
@@ -0,0 +1,36 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+const li = document.createElement('li');
+
+const delButton = document.createElement('button');
+
+// li.textContent = input.value;
+
+// delButton.textContent = '❌';
+
+// li.append(delButton);
+
+// list.append(li);
+
+button.addEventListener('click', function(){
+    if (input.value.trim() !== ''){
+
+        li.textContent = input.value;
+
+        delButton.textContent = '❌';
+
+        li.append(delButton);
+
+        list.append(li);
+    }
+})
+
+delButton.addEventListener('click', function(){
+    list.removeChild(li);
+    input.focus();
+})
+
+input.value='';
+input.focus();
